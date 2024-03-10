8d8cec48f5f8736769ce851116bc80577bc14ca3
Matthew Steeples <matthew@ledgerscope.com>
2024-03-10 01:12:34 +0000

    Wire up edit button

--- Shared/TrackedItem.cs
+++ Shared/TrackedItem.cs
@@ -13,6 +13,7 @@ namespace BlazorApp.Shared
             Name = name;
         }
 
+        public Guid Id { get; set; } = Guid.NewGuid();
         public string Name { get; set; }
         public string Category { get; set; }
         public ICollection<Occurrence> PastOccurrences { get; set; } = new List<Occurrence>();
