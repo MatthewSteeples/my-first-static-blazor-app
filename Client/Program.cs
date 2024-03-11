8c015678bd0f5a5568b0455325328f6485068388
Matthew Steeples <matthew@ledgerscope.com>
2024-03-11 23:47:17 +0000

    Add source gen serialization context

--- Client/Program.cs
+++ Client/Program.cs
@@ -9,7 +9,10 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
+
+//builder.Services.AddBlazoredLocalStorage(options => { options.JsonSerializerOptions.TypeInfoResolver = SerializationContext.Default; }); //Need to wait for https://github.com/Blazored/LocalStorage/pull/241
 builder.Services.AddBlazoredLocalStorage();
+
 builder.Services.AddFluentUIComponents();
 
 await builder.Build().RunAsync();
---
+++ Client/SerializationContext.cs
@@ -0,0 +1,16 @@
+﻿using BlazorApp.Shared;
+using System.Text.Json.Serialization;
+
+namespace BlazorApp.Client
+{
+    [JsonSourceGenerationOptions(WriteIndented = false)]
+
+    [JsonSerializable(typeof(TrackedItem))]
+    [JsonSerializable(typeof(Target))]
+    [JsonSerializable(typeof(Occurrence))]
+    
+    public partial class SerializationContext : JsonSerializerContext
+    {
+        
+    }
+}
