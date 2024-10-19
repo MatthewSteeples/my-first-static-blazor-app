2d062474c732bdcec76db220363a71f9c0721813
Matthew Steeples <matthew@ledgerscope.com>
2024-10-19 19:42:08 +0100

    First pass at notifications

---
+++ Api/NotificationScheduler.cs
@@ -0,0 +1,55 @@
+﻿using BlazorApp.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.DurableTask;
+using Microsoft.DurableTask.Client;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using WebPush;
+
+namespace Api
+{
+    public class NotificationScheduler
+    {
+        private readonly ILogger<NotificationScheduler> _logger;
+        private readonly IConfiguration _configuration;
+
+        public NotificationScheduler(ILogger<NotificationScheduler> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [Function("Notification")]
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")] NotificationSubscription notificationSubscription, [DurableClient] DurableTaskClient client)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            await client.ScheduleNewOrchestrationInstanceAsync(nameof(NotificationWorkflow), notificationSubscription);
+
+            return new OkObjectResult("Welcome to Azure Functions!");
+        }
+
+        [Function(nameof(NotificationWorkflow))]
+        public async Task NotificationWorkflow(
+            [OrchestrationTrigger] TaskOrchestrationContext context,
+            NotificationSubscription input)
+        {
+            await context.CreateTimer(input.TargetTime, CancellationToken.None);
+
+            var section = _configuration.GetSection("VapidKeys");
+            var vapidPublicKey = section["PublicKey"];
+            var vapidPrivateKey = section["PrivateKey"];
+
+            var pushSubscription = new PushSubscription(
+                input.Url,
+                input.P256dh,
+                input.Auth);
+
+            var vapidDetails = new VapidDetails("mailto:example@tracker.p4nda.co.uk", vapidPublicKey, vapidPrivateKey);
+
+            using (var webPushClient = new WebPushClient())
+                await webPushClient.SendNotificationAsync(pushSubscription, input.Message, vapidDetails);
+        }
+    }
+}
---
+++ Shared/NotificationSubscription.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BlazorApp.Shared;
+
+public class NotificationSubscription
+{
+    public int? NotificationSubscriptionId { get; set; }
+
+    public string? UserId { get; set; }
+
+    public string? Url { get; set; }
+
+    public string? P256dh { get; set; }
+
+    public string? Auth { get; set; }
+
+    public DateTime TargetTime { get; set; }
+
+    public string Message { get; set; }
+}
