aa2be5cf4c002d0911297fa99715a25a717ff2a5
Matthew Steeples <matthew@ledgerscope.com>
2025-06-22 18:54:25 +0100

    Add wasm-tools

--- .cloudflare/build.sh
+++ .cloudflare/build.sh
@@ -2,5 +2,6 @@
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 9.0 -InstallDir ./dotnet
+./dotnet/dotnet workload install wasm-tools
 ./dotnet/dotnet --version
 ./dotnet/dotnet publish ../Client/Client.csproj -c Release -o ./public
