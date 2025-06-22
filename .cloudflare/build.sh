982698f26f6de19604cf46899c86802b2092a5de
Matthew Steeples <matthew@ledgerscope.com>
2025-06-22 18:48:36 +0100

    Relative path

--- .cloudflare/build.sh
+++ .cloudflare/build.sh
@@ -3,4 +3,4 @@ curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 9.0 -InstallDir ./dotnet
 ./dotnet/dotnet --version
-./dotnet/dotnet publish ./Client/Client.csproj -c Release -o weathered-base-bad8/public
+./dotnet/dotnet publish ../Client/Client.csproj -c Release -o ./public
