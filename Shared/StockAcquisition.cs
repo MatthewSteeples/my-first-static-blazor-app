d8eebeb3e39d13638ded5e7de2e4d8cb894b9e7b
Matthew Steeples <matthew@ledgerscope.com>
2025-08-24 21:48:31 +0100

    Make "Stock used" narrower

--- Shared/StockAcquisition.cs
+++ Shared/StockAcquisition.cs
@@ -1,9 +1,11 @@
+using System;
+
 namespace BlazorApp.Shared
 {
     public class StockAcquisition
     {
         public DateTime DateAcquired { get; set; }
         public int Quantity { get; set; }
-        public string? Note { get; set; }
+        public string Note { get; set; }
     }
 }
